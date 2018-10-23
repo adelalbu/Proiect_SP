39a5e25f3c690873342de1430519b776406db700
Adela <Adela@DESKTOP-VAIETT2>
2018-10-23 23:43:32 +0300

    proxy

--- Proiect_SP/src/withcomposite/MainClass.java
+++ Proiect_SP/src/withcomposite/MainClass.java
@@ -1,11 +1,18 @@
+
 package withcomposite;
+
 public class MainClass {
 
   public static void main(String[] args) {
     Carte noapteBuna = new Carte("Noapte buna, copii!");
- 
-
     Section cap1 = new Section("Capitolul 1");
+
+    ImagineProxy img1 = new ImagineProxy("casa.jpg");
+    cap1.add(img1);
+
+    ImagineProxy img2 = new ImagineProxy("masina.jpg");
+    cap1.add(img2);
+
     cap1.add(new Text("Text 1"));
     cap1.add(new Text("Text 2"));
     cap1.add(new Imagine("Imagine 1"));
@@ -13,7 +20,6 @@ public class MainClass {
     noapteBuna.add(new Text("Multumesc"));
     noapteBuna.print();
 
-    
   }
 
 }
