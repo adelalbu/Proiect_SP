190ecc398f0b1962890ad440eafe39013b6e7f8b
adela <adela@10.3.0.199>
2018-11-19 16:46:29 +0200

    aaaaaaaaaaa

--- Proiect_SP/src/builder/Builder.java
+++ Proiect_SP/src/builder/Builder.java
@@ -1,83 +1,24 @@
 package builder;
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
+public interface Builder {
 
-public class Builder implements IBuilder {
-  private String fileName;
-  private Element document;
+  Element getResult();
 
-  public Builder(String fileName) {
-    this.fileName = fileName;
-  }
+  void build() throws JsonParseException, JsonMappingException, IOException;
 
-  @Override
-  public Element getResult() {
-    return document;
-  }
+  Element buildImage(HashMap<String, Object> map);
 
-  @Override
-  public void build() throws JsonParseException, JsonMappingException, IOException {
-    ObjectMapper mapper = new ObjectMapper();
-    @SuppressWarnings("unchecked")
-    HashMap<String, Object> map = mapper.readValue(new File(fileName), HashMap.class);
-    //readValue(map);
-    for (Map.Entry<String, Object> entry : map.entrySet()) {
-      String key = entry.getKey();
-      if (key.equals("class")) {
-        readValue(map, key);
-      }
-      Object value = entry.getValue();
-      System.out.println(key + " --- " + value);
-    }
-  }
+  Element buildParagraph(HashMap<String, Object> map);
 
-  public void readValue(HashMap<String, Object> map, String classType) {
-    switch (classType) {
-      case "Section":
-        document.add(buildSection(map));
-        break;
-      case "Image":
-        document.add(buildImage(map));
-        break;
-      case "Paragraph":
-        document.add(buildParagraph(map));
-        break;
-    }
-  }
+  Element buildTable(HashMap<String, Object> map);
 
-  @Override
-  public Element buildImage(HashMap<String, Object> map) {
-    Imagine image = new Imagine((String) map.get("url"));
-    return image;
-  }
+  Element buildImageProxy(HashMap<String, Object> map);
 
-  @Override
-  public Element buildParagraph(HashMap<String, Object> map) {
-    Text paragraph = new Text((String) map.get("text"));
-    return paragraph;
-  }
+  Element buildSection(HashMap<String, Object> map);
 
-  @Override
-  public Element buildTable(HashMap<String, Object> map) {
-    return null;
-  }
-
-  @Override
-  public Element buildImageProxy(HashMap<String, Object> map) {
-    return null;
-  }
-
-  @Override
-  public Element buildSection(HashMap<String, Object> map) {
-    Section section = new Section((String) map.get("title"));
-    readValue(map, (String) map.get("class"));
-    return section;
-  }
 }
--- Proiect_SP/src/builder/MainClass.java
+++ Proiect_SP/src/builder/MainClass.java
@@ -1,15 +1,13 @@
 package builder;
 
-
 import java.io.IOException;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
-
 public class MainClass {
   public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
-    Builder jsonBuilder = new Builder("book.json");
+    JSONBuilder jsonBuilder = new JSONBuilder("book.json");
     jsonBuilder.build();
 
     Element myBook = jsonBuilder.getResult();
